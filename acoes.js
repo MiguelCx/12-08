1d18b8d2516ad0750deff07b4de43074007dd4d4
Miguel <105358771+MiguelCx@users.noreply.github.com>
2024-08-12 08:28:12 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,6 @@
+
+ //resp.innerHTML += '<p>Clicou no primeiro botão</p>'
+ //let nome = window.prompt('Qual é o seu nome?')
+ //       let res = window.document.getElementById('saida')
+ //      res.innerHTML += `<p>Olá, <strong>${nome}</strong>! É
+ //          um grande prazer te conhecer! &#x1F596;`
